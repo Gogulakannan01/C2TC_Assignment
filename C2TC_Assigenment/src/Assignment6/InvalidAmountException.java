54324b220a988f80b49579545a6f23d06e2956ca
acer <acer@DESKTOP-2DB6O0B>
2025-08-01 14:00:30 +0530

    six

--- C2TC_Assigenment/src/Assignment6/InvalidAmountException.java
+++ C2TC_Assigenment/src/Assignment6/InvalidAmountException.java
@@ -1,5 +1,7 @@
 package Assignment6;
 
-public class InvalidAmountException {
-
+public class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
 }
